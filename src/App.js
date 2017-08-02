5f81d21f7e6cf4943464e81a8f1e794364c47ad6
localadmin <localadmin@localadmins-iMac-2.local>
2017-08-02 09:57:54 -0500

    import all components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import CompozeForm from './CompozeForm'
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+
+
+  render() {
+    return (
+     <CompozeForm />
+    );
+  }
+}
+
+
